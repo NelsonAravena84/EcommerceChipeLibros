7e19200beb5c888d67ffab17c112951e427cb01d
nelson <nelsonaravena2024@gmail.com>
2025-11-05 14:24:54 -0300

    feature: se agrego la url de cloudflare

--- app/api/productosApi.ts
+++ app/api/productosApi.ts
@@ -1,27 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // apuntando a /api
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 export const obtenerProductos = async () => {
-    try {
-        const response = await api.get('/productos'); // GET http://localhost:3000/api/productos
-        return response.data;
-    } catch (error) {
-        console.error('Error al obtener productos:', error);
-        throw error;
-    }
-}
-
-export const obtenerProductosPorCategoria = async (categoriaId: number) => {
   try {
-    const response = await api.get('/productos/por-categoria', {
-      params: { categoria: categoriaId }
-    });
+    const response = await api.get('/productos');
     return response.data;
   } catch (error) {
     console.error('Error al obtener productos:', error);
     throw error;
   }
-}
+};
+
+export const obtenerProductosPorCategoria = async (categoriaId: number) => {
+  try {
+    const response = await api.get('/productos/por-categoria', {
+      params: { categoria: categoriaId },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error al obtener productos por categoría:', error);
+    throw error;
+  }
+};
